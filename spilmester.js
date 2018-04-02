6c89ae86fa9d6d552c11614eeddff397b357186d
wowowowowowowow <38004907+wowowowowowowow@users.noreply.github.com>
2018-04-02 21:58:17 +0200

    Update spilmester.js

--- spilmester.js
+++ spilmester.js
@@ -1,19 +1,15 @@
-const discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log("er i klar!!!");
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.on("message", (message) => {
-  if (message.content.startsWith("!historie")) {
-    message.channel.send("Jeg er under om programering aldrig brug commando til discord.js");
-  } else
-  
-  if (message.content.startsWith("!comp")) {
-      message.channel.send("Jeg er under om programering aldrig brug commando til discord.js");
-  }
-  
+client.on('message', message => {
+    if (message.content === '!historie') {
+    	message.reply('Under omprogramering');
+  	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
