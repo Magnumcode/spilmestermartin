8b1c5216b8f40fd3dbfdf72fd4f521aa96e0fd1e
wowowowowowowow <38004907+wowowowowowowow@users.noreply.github.com>
2018-04-02 21:12:28 +0200

    Update spilmester.js

--- spilmester.js
+++ spilmester.js
@@ -1,23 +1,10 @@
-//const commando = require("discord.js-commando");
-//const client = new commando.Client({
-//    owner: '308479121489657857'
-//});
-
-//client.registry.registerGroup("spille", "Spille");
-//client.registry.registerDefaults();
-//client.registry.registerCommandsIn(__dirname + "/commands");
-
-const Discord = require('discord.js-commando');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
+const commando = require("discord.js-commando");
+const client = new commando.Client({
+    owner: '308479121489657857'
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+client.registry.registerGroup("spille", "Spille");
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(__dirname + "/commands");
 
 client.login(process.env.BOT_TOKEN);
