7c84c523d1d71c5ee4553ce648e72f08fc3edef1
wowowowowowowow <38004907+wowowowowowowow@users.noreply.github.com>
2018-04-03 14:45:28 +0200

    Update spilmester.js

--- spilmester.js
+++ spilmester.js
@@ -8,6 +8,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!historie') {
+        historie = Math.floor(Math.random() * 3) + 1;
     	message.reply(historie);
   	}
 });
