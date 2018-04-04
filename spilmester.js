e96c3737ba432f040fe2a387b86288aae1170fdc
wowowowowowowow <38004907+wowowowowowowow@users.noreply.github.com>
2018-04-04 11:18:12 +0200

    Update spilmester.js

--- spilmester.js
+++ spilmester.js
@@ -35,8 +35,19 @@ client.on("message", message => {
     
         if (historie == 6) {
             message.channel.send("@everyone Du skal spille en comp. Du er i en lobby med Nicolas A (den sindssyge glock-18 spiller som løber i midten af alting) og Lukas (ham der købte buttplugs for 70 €). Lukas har lavet lobbyen og vil gerne have dust 2 på. Du gider ikke at have dust 2 på fordi du har en speedygon dell som ikke kan trække dust 2. Lukas siger at han har fjernet Dust 2 og har sat Mirage på. Du accepterer kampen men finder ud af at det bliver Dust 2. Vil du vælge at leave kampen eller råbe \"Din fede autist! Dø i et hul!\" til Lukas?");
-        }
-  	}
+        
+	if (historie == 7) {
+	    message.channel.send("@everyone Ny historie til spilmester: det er klokken 7 om morgenen. Du har lige været til Lan hos Asbjørn til kl 2 om natten. Du vågner ved at Asbjørn skriver om nogen spille morgen comp. Du har ikke andet at lave så du ville selvfølgelig gerne. Asbjørn vælger agency og nuke som maps. Det har du ikke sagt ja til men han starter alligevel. Når du er logget ind på serveren hører du en lille russer dreng der råber BLYAT. måske den samme russer unge som du spillede med i går. Du køber negev og starter et global elite spray. Lige pludselig ser du den lille russer knægt hoppe ind foran skærmen. det næste du ser er et skilt hvor der står: 6 days 23 hours 59 minutes.")
+	}
+		
+	if (historie == 8) {
+	    message.channel.send("@everyone Du er lige logget ind i serveren. en af dine teammates taler kun tysk. vælger du at sige den magiske remse: sie bist bøsse und knalden mit dem tyr eller siger du heil Hitler?")
+	}
+	
+	if (historie == 9) {
+	    message.channel.send("@everyone du er i comp med Alex. han køber to flash-bangs. Du vælger at gå palace og lige da du peaker misser Alex sin flash. Vil du vælge at skyde ham eller skyde ham?")
+	}		
+   }
    
    //sender en besked til hele serveren og spørger om der er nogen der vil spille comp med en tilfældig besked
    if (message.content == "!comp") {
