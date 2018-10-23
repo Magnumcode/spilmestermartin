c3a2aa48ffb21dfd047c96f165a340ede3df1d0f
wowowowowowowow <38004907+wowowowowowowow@users.noreply.github.com>
2018-10-23 15:35:00 +0200

    Update spilmester.js

--- spilmester.js
+++ spilmester.js
@@ -146,7 +146,7 @@ client.on("message", message => {
    }
 	
    if (message.content == message.content.includes(!roll)) {
-       var roll_number = message.content.replace("!roll", "");
+       //var roll_number = message.content.replace("!roll", "");
        message.channel.send(roll_number);
    }
 });
